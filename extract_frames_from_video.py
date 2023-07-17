c677304a48f8be12f636eaae29b5f1eb4d7d39ac
kitano <kitano>
2023-07-17 16:44:35 +0900

    fix

--- extract_frames_from_video.py
+++ extract_frames_from_video.py
@@ -12,6 +12,7 @@ script_path = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_path)
 model_path = os.path.join(script_dir, "models", "keypoint.pt")
 TARGET_CLASS = 0
+THR_PIXEL = 30
 
 def get_video_paths(path):
     video_extensions = [".MP4", ".mp4", ".avi", ".mkv", ".MOV"]
@@ -44,6 +45,7 @@ def extract_frames_from_video(input, output) -> None:
         count = 0
         frame_idx = 0
         pos_curr = np.array([0, 0])
+        before_not_detected_flag = False
         while True:
             ret, frame = cap.read()
             if ret:
@@ -58,12 +60,13 @@ def extract_frames_from_video(input, output) -> None:
                 ]
 
                 if len(target_idxs) > 0:
+                    before_not_detected_flag = False
                     idx = np.argmax(boxes.conf[target_idxs])
 
                     box_x, box_y, box_width, box_height = boxes.xywhn[idx]
                     toe, hosel, grip = keypoints.xy[idx]
 
-                    if np.linalg.norm(toe - pos_curr) > 10:
+                    if np.linalg.norm(toe - pos_curr) > THR_PIXEL:
                         pos_curr = toe
                         frame_idx += 1
                         frame_path = os.path.join(
@@ -71,14 +74,18 @@ def extract_frames_from_video(input, output) -> None:
                         )
                         cv2.imwrite(frame_path, frame)
                 else:
-                    frame_idx += 1
-                    frame_path = os.path.join(
-                        output_dir, f"{video_name}_{frame_idx}.jpg"
-                    )
-                    cv2.imwrite(frame_path, frame)
+                    if not before_not_detected_flag:
+                        frame_idx += 1
+                        frame_path = os.path.join(
+                            output_dir, f"{video_name}_{frame_idx}.jpg"
+                        )
+                        cv2.imwrite(frame_path, frame)
+                        before_not_detected_flag = True
+                    else:
+                        pass
             else:
-                printt("")
-                logging.info(f"Finished processing {video_path}")
+                print("")
+                logging.info(f"Finished processing {video_path}: ")
                 break
 
 if __name__ == "__main__":
